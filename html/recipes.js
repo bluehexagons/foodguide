e85d3cd9e0f950cdac65c5999e98401694550569
lakhnishMonster <lakhnishmonster@gmail.com>
2023-08-04 12:26:55 -0400

    Revert "remove dst recipes since site broke"
    This reverts commit 7192f78d24f428c333b30b3d4cdaa21f24cdbf79.

--- html/recipes.js
+++ html/recipes.js
@@ -1439,7 +1439,6 @@ export const recipes = {
 		mode: 'giants'
 	},
 	REMOVE AFTER UPLOADING SECTION DIVIDERS*/
-	/*
 	jellybean: {
 		name: 'Jellybeans',
 		test: (cooker, names, tags) => {
@@ -1893,7 +1892,7 @@ export const recipes = {
 	// 	note: 'Used to feed Dust Moths, cannot be eaten by the player',
 	// 	mode: 'together'
 	// }
-	*/
+	
 //--------------------------------------------------------------------------------\\	
 //	                 DON'T STARVE TOGETHER WARLY RECIPES                          \\
 //--------------------------------------------------------------------------------\\

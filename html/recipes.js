cb5dd5e46ecc8a9a0daffb3880b640d1eefbbaab
lakhnishMonster <lakhnishmonster@gmail.com>
2023-08-05 20:57:57 -0400

    Winter Koalefant Trunks + Live Pond Eel
    Added the winter koalefant trunk.
    Added the meat value to all koalefant trunks in DST.
    Added pondeel ingredient and adjusted unagi recipe for to include pondeel.

--- html/recipes.js
+++ html/recipes.js
@@ -1337,7 +1337,7 @@ export const recipes = {
 	unagi_dst: {
 		name: 'Unagi',
 		test: (cooker, names, tags) => {
-			return (names.cutlichen_dst || names.kelp || names.kelp_cooked || names.kelp_dried) && (names.eel_dst || names.eel_cooked_dst);
+			return (names.cutlichen_dst || names.kelp || names.kelp_cooked || names.kelp_dried) && (names.eel_dst || names.eel_cooked_dst || names.pondeel);
 		},
 		requirements: [OR(NAME('cutlichen'),NAME('kelp')), NAME('eel')],
 		priority: 20,
@@ -1785,7 +1785,7 @@ export const recipes = {
 		test: (cooker, names, tags) => {
 			return (names.trunk_summer_dst || names.trunk_cooked_dst || names.trunk_winter_dst) && (names.fig || names.fig_cooked);
 		},
-		requirements: [NAME('trunk_summer'), NAME('fig')],
+		requirements: [NAME('trunk'), NAME('fig')],
 		priority: 40,
 		foodtype: 'meat',
 		health: healing_huge,
